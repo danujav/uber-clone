5267a1e788ee895c5925628fff624f5e773a9d16
DanujaV <danujagreru@gmail.com>
2021-08-28 22:49:30 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Provider store={}>
+      <View style={styles.container}>
+      <Text>I'm gonna creating uber app clone today!</Text>
     </View>
+    </Provider>
+    
   );
 }
 
